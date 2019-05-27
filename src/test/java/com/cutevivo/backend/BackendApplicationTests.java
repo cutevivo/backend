e401c5aa0acb234ef26a430797ee16ba3307563e
Yuiheart <151250023@smail.nju.edu.cn>
2019-05-27 08:20:12 +0800

    5/27 8:19
    清除evosuite的测试用例

--- src/test/java/com/cutevivo/backend/BackendApplicationTests.java
+++ src/test/java/com/cutevivo/backend/BackendApplicationTests.java
@@ -1,20 +1,20 @@
 package com.cutevivo.backend;
 
-import com.cutevivo.backend.Entity.CollectionEntry;
+
 import com.cutevivo.backend.Entity.Course;
-import com.cutevivo.backend.Entity.Note;
 import com.cutevivo.backend.Entity.User;
 import com.cutevivo.backend.Service.CollectionEntryService;
 import com.cutevivo.backend.Service.CourseService;
 import com.cutevivo.backend.Service.NoteService;
 import com.cutevivo.backend.Service.UserService;
-import com.cutevivo.backend.ServiceImpl.UserServiceImpl;
 import com.cutevivo.backend.utils.ResultMessage;
-import org.junit.Test;
+
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
+import org.junit.Test;
+
 
 import java.util.List;
 
@@ -213,5 +213,7 @@ public class BackendApplicationTests {
     public void test21(){
 
     }
+
+
 
 }
